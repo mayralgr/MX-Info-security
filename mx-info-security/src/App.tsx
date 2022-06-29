9d5d37b9fb5c9a6ad7125b8b45def8ff909101e0
Mayra Lucero Garcia Ramírez <ml17ft@gmail.com>
2022-06-28 21:58:49 -0500

    Add map:

--- mx-info-security/src/App.tsx
+++ mx-info-security/src/App.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { SyntheticEvent } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MexicoMap from "./resources/images/states/mapchart2.svg";
 
-function App() {
+const App = () => {
+
+  const handleClick = (e: any) => {
+    const target = e.target;
+    debugger;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <p>
-        Let´s do this
-      </p>
+    <div className="App" onClick={(e) => handleClick(e)} style={{ width: "100%" }}>
+      {/* let state = document.getElementById("map").getSVGDocument().getElementById("MX-BCN")
+undefined
+state.style.fill = "blue"
+'blue' */}
+      <embed id="map" onClick={(e) => handleClick(e)}  style={{ width: "100%" }} src={MexicoMap} />
     </div>
   );
 }
